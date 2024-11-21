345ddbcc3a959ec932d60406a59b5318441eebf1
clovis-fag <ckschweinberger@minha.fag.edu.br>
2024-11-21 10:00:38 -0300

    vite config + form

--- reactproject/src/components/Contact.tsx
+++ reactproject/src/components/Contact.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState} from "react";
 import "../styles/contact.css";
 
 const Contact: React.FC = () => {
+    const [email, setEmail] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+    
+        const body = {
+            toMail: email,
+            content: content,
+        };
+    
+        try {
+            await fetch("/api", { 
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ5NzQwYTcwYjA5NzJkY2NmNzVmYTg4YmM1MjliZDE2YTMwNTczYmQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA3MTg4MjgxNzAzOTc3MTQ5MjMzIiwiaGQiOiJtaW5oYS5mYWcuZWR1LmJyIiwiZW1haWwiOiJja3NjaHdlaW5iZXJnZXJAbWluaGEuZmFnLmVkdS5iciIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiS2xTTHRmbnpkT2xqenUxZjdBR1l0QSIsImlhdCI6MTczMjE5MjUwOSwiZXhwIjoxNzMyMTk2MTA5fQ.YeziVW9ocejQhTAEGpm_oIzBZbIpdt0lZQUtUwP8ULlHFsY9bq9vI2S802aZBpfxqxVrV6RCFqu35vRnEFZfbhEdyKjwERblBZM9tsVc5MMPMLS6Ymx2mltX6JYuKEQzWyS7s_KLkFvzvGihKZ5GeyVjlJFRwBrlZDs9HjeKKAr1t8jJ5tuU-M_Zp11k9cilJK4mgHeaUVsvjJNpleQPn6JawSpv2D3SfgJmSLsRYZ2pLWwUMncqA7p5h0zZiQawS1xvdpitAL_M2Z12WIm7vouh5SZjoUlsH_BVKHnReVHIixXC-txmJnG1P3tQyAmiWbtssY6z7CMffrVt6yQVNw",
+                },
+                body: JSON.stringify(body),
+            });
+
+        } catch (error) {
+            alert("Ocorreu um erro ao enviar o formulário.");
+            console.error("Erro na requisição:", error);
+        }
+    };
+
     return (
         <section id="contact" className="contact-section">
             <header>
@@ -12,17 +39,21 @@ const Contact: React.FC = () => {
                 Nossa equipe está pronta para atender você com dedicação e carinho.</p>
             </header>
 
-            <form className="contact-form">
+            <form className="contact-form" onSubmit={handleSubmit}>
                 <input
                     type="email"
                     placeholder="Seu melhor Email"
                     className="form-input"
                     required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <textarea
                     placeholder="Motivo do contato. Ex: Gostaria de agendar uma consulta ou saber mais sobre o tratamento X."
                     className="form-textarea"
                     required
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                 ></textarea>
                 <button type="submit" className="form-button">Enviar</button>
             </form>
--- reactproject/vite.config.ts
+++ reactproject/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://southamerica-east1-lambda-arch.cloudfunctions.net/function-1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
